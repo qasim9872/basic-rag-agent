1d531a560c57540fdcbbc4b9f76561e4587002d6
Qasim <qasim9872@gmail.com>
2024-08-09 22:35:03 +0100

    update

--- lib/actions/resources.ts
+++ lib/actions/resources.ts
@@ -6,18 +6,28 @@ import {
   resources,
 } from "@/lib/db/schema/resources";
 import { db } from "../db";
+import { generateEmbeddings } from "../ai/embedding";
+import { embeddings as embeddingsTable } from "../db/schema/embeddings";
 
 export const createResource = async (input: NewResourceParams) => {
   try {
-    const payload = insertResourceSchema.parse(input);
+    const { content } = insertResourceSchema.parse(input);
 
-    const contentWithoutLineBreaks = payload.content.replace("\n", " ");
+    const contentWithoutLineBreaks = content.replace("\n", " ");
     const [resource] = await db
       .insert(resources)
       .values({ content: contentWithoutLineBreaks })
       .returning();
 
-    return "Resource successfully created.";
+    const embeddings = await generateEmbeddings(content);
+    await db.insert(embeddingsTable).values(
+      embeddings.map((embedding) => ({
+        resourceId: resource.id,
+        ...embedding,
+      }))
+    );
+
+    return 'Resource successfully created and embedded.';
   } catch (e) {
     if (e instanceof Error)
       return e.message.length > 0 ? e.message : "Error, please try again.";
